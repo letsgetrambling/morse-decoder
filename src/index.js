cbd3c02509cb6bc4558b2a100c32ff68bd1da435
Konstantsin <konstantinlemesh@yandex.by>
2023-10-03 14:42:33 +0300

    feat: add morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,56 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrTen = [];
+
+for ( let i = 0; i < expr.length; i = i + 10) {
+  let ten = expr.slice(i, i + 10);
+arrTen.push(ten);
+}
+
+let arrWZ = [];
+for ( let i = 0; i < arrTen.length; i++) {
+if (arrTen[i] === '**********' ) {
+  arrWZ.push(' ');
+} else {
+  for ( let j = 0; j < arrTen[i].length; j = j + 2) {
+    if ( arrTen[i][j] !== '0') {
+      arrWZ.push(arrTen[i].slice(j));
+      break;
+    } 
+  }
+}
+}
+
+
+let arrDotDash = [];
+
+for ( let i = 0; i < arrWZ.length; i++) {
+  let stringI = [];
+  for ( let j = 0; j < arrWZ[i].length; j = j + 2) {
+  if (arrWZ[i].slice(j, j + 2) === '11') {
+    stringI = stringI + '-';
+  } else if (arrWZ[i].slice(j, j + 2) === '10') {
+    stringI = stringI + '.';
+  } else stringI = stringI + arrWZ[i];
+}
+arrDotDash.push(stringI);
+}  
+
+let finalString = '';
+
+for ( let i = 0; i < arrDotDash.length; i++) {
+  if (arrDotDash[i] === ' ') {
+    finalString = finalString + ' ';
+  }
+   else {
+   } for ( let key in MORSE_TABLE) {
+if ( arrDotDash[i] === key) {
+finalString = finalString + MORSE_TABLE[key];
+} 
+  }
+}
+return finalString;
 }
 
 module.exports = {
